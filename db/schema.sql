60801422befdaa09620bdd12bdc29ab935c1ebb9
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-10-29 15:06:36 -0500

    joining tables

--- db/schema.sql
+++ db/schema.sql
@@ -20,5 +20,6 @@ CREATE TABLE employee (
   first_name VARCHAR (30),
   last_name VARCHAR (30),
   role_id INTEGER,
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
